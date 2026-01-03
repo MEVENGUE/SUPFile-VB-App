71b775b8070c84d5a7aaf9fb070bfb1e85d33ab6
MEVENGUE FRANCK <117992715+MEVENGUE@users.noreply.github.com>
2026-01-03 16:15:40 +0100

    Remove unused import of useAuth from AuthContext

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -1,5 +1,4 @@
 import { useState, useRef, useEffect } from 'react'
-import { useAuth } from '../contexts/AuthContext'
 import axios from 'axios'
 import Sidebar from '../components/Sidebar'
 import './ChatPage.css'
